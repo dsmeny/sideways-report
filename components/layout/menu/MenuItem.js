639faaf4696bd8c3cab3216b260377c773d57fe1
dsme <bradleyrubin106@gmail.com>
2021-08-02 12:37:58 -0400

    changing search color and appearance

--- components/layout/menu/MenuItem.js
+++ components/layout/menu/MenuItem.js
@@ -5,13 +5,33 @@ const MenuItem = (props) => {
     <>
       <ul className={classes.list_styles}>
         <li
-          className={(props.searchTrigger && classes.active) || ""}
           onClick={(e) => {
-            e.target.classList.toggle(classes.active);
+            e.currentTarget.classList.toggle(classes.active);
             props.classTriggerHandler();
+            e.stopPropagation();
           }}
         >
-          Search
+          <span
+            style={{
+              transform: "translateX(-10px)",
+            }}
+          >
+            Search
+          </span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            style={{ height: "2rem" }}
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            />
+          </svg>
         </li>
         {/* <li>p2</li>
         <li>p3</li> */}

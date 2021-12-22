2c6f4563e2330188d3a17119a36fe65c2841c8cf
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2021-12-22 05:23:17 -0500

    fixing rounding problem

--- components/utility/general.js
+++ components/utility/general.js
@@ -2,8 +2,8 @@ export const convertNumber = (param) => {
   if (/[A-Z,a-z]/g.test(param)) {
     return param;
   } else {
-    return new Intl.NumberFormat("en-US", {
-      maximumSignificantDigits: 3,
-    }).format(param);
+    return new Intl.NumberFormat()
+      .formatToParts(param)
+      .reduce((string, part) => string + part.value, "");
   }
 };

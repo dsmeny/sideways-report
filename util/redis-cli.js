b85e28d75040fbdf6cb6c9336d07d06c64fa2b70
DSme <bradleyrubin106@gmail.com>
2022-01-15 09:44:24 -0500

    Delete redis-cli.js

--- util/redis-cli.js
+++
@@ -1,9 +0,0 @@
-const Redis = require("ioredis");
-
-const redis = new Redis({
-  host: process.env.NEXT_PUBLIC_REDIS_URL,
-  port: process.env.NEXT_PUBLIC_REDIS_PORT,
-  password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
-});
-
-export default redis;

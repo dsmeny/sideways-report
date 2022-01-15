68ac4edf1e736b73966d52f2c591aa57e2620f5f
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2022-01-15 09:34:14 -0500

    fixing api keys

--- util/redis-cli.js
+++ util/redis-cli.js
@@ -1,9 +1,9 @@
 const Redis = require("ioredis");
 
 const redis = new Redis({
-  host: "redis-11050.c114.us-east-1-4.ec2.cloud.redislabs.com",
-  port: 11050,
-  password: "1X2NGBocH9xt8bsWzYXnmLVd8bJEqkXs",
+  host: process.env.NEXT_PUBLIC_REDIS_URL,
+  port: process.env.NEXT_PUBLIC_REDIS_PORT,
+  password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
 });
 
 export default redis;

1dda0705532b1ef878340b34786d45a9f2efab4d
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2022-03-24 19:54:49 -0400

    upstash env vars

--- pages/api/redis_cloud.js
+++ pages/api/redis_cloud.js
@@ -9,7 +9,6 @@ export default async function handler(req, res) {
   } else {
     const { name } = req.query;
     const response = await redis.get(name);
-    // console.log("Response redis_cloud: ", response);
     res.status(200).json({ message: "ok", data: response });
   }
 }

414e85616df0bde096577bed55145613dc2ed40e
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2022-03-31 06:19:48 -0400

    mobile responsive for nav

--- components/layout/menu/MenuItem.js
+++ components/layout/menu/MenuItem.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from "react";
 import classes from "./MenuBar.module.css";
 import { RiSendPlaneFill } from "react-icons/ri";
 
@@ -6,12 +7,20 @@ const icon = {
 };
 
 const MenuItem = ({ searchTrigger, searchHandler }) => {
+  const [isMobile, setIsMobile] = useState(null);
+
+  useEffect(() => {
+    setIsMobile(() => window.matchMedia("(max-width: 720px)").matches);
+  });
+
+  console.log("isMobile: ", isMobile);
+
   return (
     <div
       onClick={searchHandler}
       className={`${classes.title} ${searchTrigger ? classes.active : ""}`}
     >
-      <span>which stock do you like ??</span>
+      {!isMobile && <span>which stock do you like ??</span>}
       <RiSendPlaneFill style={icon} />
     </div>
   );

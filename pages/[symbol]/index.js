1ed55b853ed4222f599c33c032c4e21456764a37
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2022-11-07 05:42:37 -0500

    vercel bug

--- pages/[symbol]/index.js
+++ pages/[symbol]/index.js
@@ -1,7 +1,7 @@
 import { useRouter } from "next/router";
-// import CompanyOverview from "../../base/components/views/Overview";
-// import TimeSeriesOverview from "../../base/components/views/History";
-// import NewsOverview from "../../base/components/views/News";
+import CompanyOverview from "../../base/components/views/Overview";
+import TimeSeriesOverview from "../../base/components/views/History";
+import NewsOverview from "../../base/components/views/News";
 import { views } from "../../constants";
 import { TimeseriesProvider } from "../../contexts/timeseries-context";
 
@@ -19,9 +19,9 @@ const Details = () => {
   return (
     <TimeseriesProvider>
       <div style={containerStyle}>
-        {/* {pageType === OVERVIEW && <CompanyOverview symbol={symbol} />} */}
-        {/* {pageType === HISTORY && <TimeSeriesOverview symbol={symbol} />} */}
-        {/* {pageType === NEWS && <NewsOverview />} */}
+        {pageType === OVERVIEW && <CompanyOverview symbol={symbol} />}
+        {pageType === HISTORY && <TimeSeriesOverview symbol={symbol} />}
+        {pageType === NEWS && <NewsOverview />}
       </div>
     </TimeseriesProvider>
   );

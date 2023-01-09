ebc2a441e5a333c973bd04ab068e12ed0078af09
Bradley Rubin <bradleyrubin@Bradleys-Mac-mini.local>
2023-01-09 10:33:14 -0500

    sr-186

--- constants.js
+++ constants.js
@@ -38,27 +38,21 @@ export const overviewModel = (stockData, convertNumber) => {
       Description: stockData.Description ?? "N/A",
     },
     row2: {
-      ["Shares Outstanding"]: convertNumber(stockData.sharesOutstanding),
-      Float: convertNumber(stockData.floatShares),
-      ["Shares Short"]: convertNumber(stockData.sharesShort),
-      ["% Shares Out"]: `${stockData.sharesPercentSharesOut * 100}%`,
+      ["Shares Outstanding"]: "Coming soon",
+      Float: "Coming soon",
+      ["Shares Short"]: "Coming soon",
+      ["% Shares Out"]: "Coming soon",
     },
     row3: {
-      ["Short Recent Month"]: convertNumber(
-        stockData.sharesShortPreviousMonthDate
-      ),
-      ["Short Prior Month"]: convertNumber(stockData.sharesShortPriorMonth),
-      ["% Short of Float"]: `${stockData.shortPercentOfFloat * 100}%`,
-      ["Short Ratio"]: stockData.shortRatio,
+      ["Short Recent Month"]: "Coming soon",
+      ["Short Prior Month"]: "Coming soon",
+      ["% Short of Float"]: "Coming soon",
+      ["Short Ratio"]: "Coming soon",
     },
     row4: {
-      ["% Held by Institutions"]: `${(
-        stockData.heldPercentInstitutions * 100
-      ).toFixed(2)}%`,
-      ["% Held by Insiders"]: `${(stockData.heldPercentInsiders * 100).toFixed(
-        2
-      )}%`,
-      ["Last Split Factor"]: stockData.lastSplitFactor ?? "N/A",
+      ["% Held by Institutions"]: "Coming soon",
+      ["% Held by Insiders"]: "Coming soon",
+      ["Last Split Factor"]: "Coming soon",
     },
     row5: {
       ["52 Week High"]: stockData["52WeekHigh"],
@@ -126,6 +120,104 @@ export const overviewModel = (stockData, convertNumber) => {
     },
   };
 };
+
+// export const overviewModel = (stockData, convertNumber) => {
+//   return {
+//     row1: {
+//       ["Latest Quarter"]: stockData.LatestQuarter,
+//       Symbol: stockData.Symbol,
+//       Name: stockData.Name,
+//       Description: stockData.Description ?? "N/A",
+//     },
+//     row2: {
+//       ["Shares Outstanding"]: convertNumber(stockData.sharesOutstanding),
+//       Float: convertNumber(stockData.floatShares),
+//       ["Shares Short"]: convertNumber(stockData.sharesShort),
+//       ["% Shares Out"]: `${stockData.sharesPercentSharesOut * 100}%`,
+//     },
+//     row3: {
+//       ["Short Recent Month"]: convertNumber(
+//         stockData.sharesShortPreviousMonthDate
+//       ),
+//       ["Short Prior Month"]: convertNumber(stockData.sharesShortPriorMonth),
+//       ["% Short of Float"]: `${stockData.shortPercentOfFloat * 100}%`,
+//       ["Short Ratio"]: stockData.shortRatio,
+//     },
+//     row4: {
+//       ["% Held by Institutions"]: `${(
+//         stockData.heldPercentInstitutions * 100
+//       ).toFixed(2)}%`,
+//       ["% Held by Insiders"]: `${(stockData.heldPercentInsiders * 100).toFixed(
+//         2
+//       )}%`,
+//       ["Last Split Factor"]: stockData.lastSplitFactor ?? "N/A",
+//     },
+//     row5: {
+//       ["52 Week High"]: stockData["52WeekHigh"],
+//       ["52 Week Low"]: stockData["52WeekLow"],
+//       ["50 Day Moving Avg"]: stockData["50DayMovingAverage"],
+//       ["200 Day Moving Avg"]: stockData["200DayMovingAverage"],
+//     },
+//     row6: {
+//       ["Market Cap (m)"]: convertNumber(stockData.MarketCapitalization),
+//       ["Book Value"]: stockData.BookValue,
+//       ["Enterprise Value"]: convertNumber(stockData.enterpriseValue),
+//       Beta: stockData.Beta,
+//     },
+//     row7: {
+//       ["Analyst Target Price"]: stockData["AnalystTargetPrice"],
+//       EPS: stockData.EPS,
+//       Industry: stockData.Industry,
+//       Sector: stockData.Sector,
+//     },
+//     row8: {
+//       GrossProfitTTM: convertNumber(stockData.GrossProfitTTM),
+//       PERatio: stockData.PERatio,
+//       PEGRatio: stockData.PEGRatio,
+//       ["Asset Type"]: stockData.AssetType,
+//     },
+//     row9: {
+//       ["Quarterly Earnings Growth YOY"]: stockData.QuarterlyEarningsGrowthYOY,
+//       ["Quarterly Revenue Growth YOY"]: stockData.QuarterlyRevenueGrowthYOY,
+//       ["Revenue Per Share TTM"]: stockData.RevenuePerShareTTM,
+//       ["Revenue TTM"]: stockData.RevenueTTM,
+//     },
+//     row10: {
+//       ["Dividend Date"]: stockData.DividendDate,
+//       ["Dividend Per Share"]: stockData.DividendPerShare,
+//       ["Dividend Yield"]: stockData.DividendYield,
+//       ["Ex Dividend Date"]: stockData.ExDividendDate,
+//     },
+//     row11: {
+//       ["Price To Sales Ratio TTM"]: stockData.PriceToSalesRatioTTM,
+//       ["Price To Book Ratio"]: stockData.PriceToBookRatio,
+//       ["Trailing PE"]: stockData.TrailingPE,
+//       ["Forward PE"]: stockData.ForwardPE,
+//     },
+//     row12: {
+//       ["Profit Margin"]: stockData.ProfitMargin,
+//       ["EV To Revenue"]: stockData.EVToRevenue,
+//       ["EV To EBITDA"]: stockData.EVToEBITDA,
+//       EBITDA: stockData.EBITDA,
+//     },
+//     row13: {
+//       ["Operating Margin TTM"]: stockData.OperatingMarginTTM,
+//       ["Return On Assets TTM"]: stockData.ReturnOnAssetsTTM,
+//       ["Return On Equity TTM"]: stockData.ReturnOnEquityTTM,
+//       ["Diluted EPS TTM"]: stockData.DilutedEPSTTM,
+//     },
+//     row14: {
+//       ["Fiscal Year End"]: stockData.FiscalYearEnd,
+//       Currency: stockData.Currency,
+//       Country: stockData.Country,
+//       Exchange: stockData.Exchange,
+//     },
+//     row15: {
+//       CIK: stockData.CIK,
+//       Address: stockData.Address,
+//     },
+//   };
+// };
 
 export const cardModel = (STOCK, toMillions) => {
   return {
